b0726cae1f4c15d2f440bead55cdb83918ffa77e
Nataly <63913225+Nataly-li@users.noreply.github.com>
2021-06-19 21:00:49 +0300

    Внесла изменения в верстку и создала файлы style_table.css и table_script.js

--- js/main.js
+++ js/main.js
@@ -1,75 +1,24 @@
-const container = document.querySelector('.container');
-const levelLinks = document.querySelectorAll('.lvl-list__link');
-const button = document.querySelector('.button');
+const listLevels = document.getElementsByClassName('list__level');
+const button = document.querySelector('.container__button');
 
-const firstLvl = document.querySelector('.first-lvl');
-const secondLvl = document.querySelector('.second-lvl');
-const thirdLvl = document.querySelector('.third-lvl');
+let levels = [];
 
-const startMenu = document.querySelector('.start-menu');
+for (i = 0; i < listLevels.length; i++) {
+  levels.push(listLevels[i].id);
+}
 
-const createCard = () => {
-  const closedCard = document.createElement('div');
-  closedCard.classList.add('img-lvl1');
-  container.appendChild(closedCard);
-  container.classList.add('flex');
-  startMenu.style.display = 'none';
-};
-
-levelLinks.forEach((levelLink) => {
-  levelLink.addEventListener('click', () => {
-    if (levelLink === firstLvl) {
-      button.addEventListener('click', () => {
-        for (i = 0; i < 3; i++) {
-          createCard();
-        }
-      });
-    }
-
-    if (levelLink === secondLvl) {
-      button.addEventListener('click', () => {
-        for (i = 0; i < 6; i++) {
-          createCard();
-        }
-      });
-    }
-
-    if (levelLink === thirdLvl) {
-      button.addEventListener('click', () => {
-        for (i = 0; i < 10; i++) {
-          createCard();
-        }
-      });
-    }
+const chooseLevelById = (ids) => {
+  ids.forEach((id) => {
+    let level = document.getElementById(id);
+    level.addEventListener('click', () => {
+      let clickToList = document.querySelectorAll('.clickToList');
+      clickToList.forEach((item) => item.classList.remove('clickToList'));
+      level.classList.add('clickToList');
+      button.onclick = () => {
+        document.location = `./table.html`;
+        localStorage.setItem('level', id);
+      };
+    });
   });
-});
-
-// const levelLinks = document.querySelectorAll('.lvl-list__link');
-// const levelItems = document.querySelectorAll('.lvl-list__item');
-
-// let rect = document.createElement('img');
-// rect.classList.add('rectangle');
-
-// levelLinks.forEach((levelLink) => {
-//   levelLink.onmouseover = (event) => {
-//     if (event) {
-//     }
-//     // let target = event.target;
-//     // target.style.color = '#d62b2e';
-//     // levelItems.forEach((levelItem) => {
-//     //   levelItem.insertBefore(rect, levelLink);
-//     // });
-//   };
-
-//   levelLink.onmouseout = (event) => {
-//     // let target = event.target;
-//     // target.style.color = '#fff';
-//     rect.remove();
-//   };
-// });
-
-// // levelLinks.forEach((levelLink) => {
-// //   levelLink.addEventListener('mouseover', () => {
-// //     levelLink.classList.add('before');
-// //   });
-// // });
+};
+chooseLevelById(levels);
---
+++ js/table_script.js
@@ -0,0 +1,69 @@
+const table = document.querySelector('#table');
+const level = localStorage.getItem('level');
+console.log('level: ', level);
+
+const isCard = false;
+
+const levels = new Map();
+levels.set('simple', 3);
+levels.set('medium', 6);
+levels.set('hard', 10);
+
+if (level === 'hard') {
+  table.classList.remove('table');
+  table.classList.add('table__hard');
+}
+
+const createCards = (isBug) => {
+  let card = document.createElement('div');
+  let inner = document.createElement('div');
+  let back = document.createElement('div');
+  let front = document.createElement('div');
+  let imgBack = document.createElement('img');
+  let imgFront = document.createElement('img');
+  imgBack.setAttribute('src', './img/card_cover.png');
+  imgFront.setAttribute(
+    'src',
+    isBug ? './img/card_bug.png' : './img/card_game_over.png'
+  );
+  front.append(imgFront);
+  back.append(imgBack);
+  inner.append(back);
+  inner.append(front);
+  card.append(inner);
+  table.append(card);
+  card.classList.add('card');
+  imgBack.classList.add('card-img__back');
+  imgFront.classList.add('card-img__front');
+
+  if (level === 'hard') {
+    inner.classList.add('card-inner__hard');
+  } else {
+    inner.classList.add('card-inner');
+  }
+
+  back.classList.add('card-inner__back');
+  front.classList.add('card-inner__front', 'reverse');
+
+  inner.onmouseover = () => back.classList.add('card__hover');
+  inner.onmouseout = () => back.classList.remove('card__hover');
+
+  card.addEventListener('click', () => {
+    if (!isCard) {
+      inner.classList.add('reverse');
+      isCard = true;
+    }
+  });
+
+  imgFront.addEventListener(
+    'click',
+    () => (document.location = './index.html')
+  );
+};
+
+const cardCount = levels.get(level);
+const bugCardIndex = Math.floor(Math.random() * cardCount);
+
+for (let i = 0; i < cardCount; i++) {
+  createCards(i === bugCardIndex);
+}
